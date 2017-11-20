67deae3092d280efa1fed093128be126be1660f1
Bronson Dunbar <bronson@bronsondunbar.com>
2017-11-20 20:39:59 +0200

    Updates

--- template-parts/navigation/navigation-navbar.php
+++ template-parts/navigation/navigation-navbar.php
@@ -23,39 +23,6 @@
 					'menu_id'        => 'navbar',
 				)); ?>
 			</div>
-
-		  	<!-- <div class="col-md-12">
-			    <ul>
-			      <li><a href="/level-one" class="navbar-content-item"><i class="fa fa-archive" aria-hidden="true"></i> Link 1 <i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>
-
-			        <ul class="submenus submenu">
-			          <li><a href="/level-two" class="navbar-content-sub-item"><i class="fa fa-bar-chart" aria-hidden="true"></i> Level 1 <i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>
-
-			            <ul class="submenus submenu">
-			              <li><a href="/level-three" class="navbar-content-sub-item"><i class="fa fa-bolt" aria-hidden="true"></i> Level 2 <i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>
-
-			              <ul class="submenus submenu">
-			                <li><a href="#"><i class="fa fa-camera" aria-hidden="true"></i> Level 3</a></li>
-			                <li><a href="#"><i class="fa fa-camera" aria-hidden="true"></i> Level 3</a></li>
-			                <li><a href="#"><i class="fa fa-camera" aria-hidden="true"></i> Level 3</a></li>
-			                <li><a href="/level-two" class="go-back">Back</a></li>
-			              </ul>
-
-			              <li><a href="#"><i class="fa fa-bolt" aria-hidden="true"></i> Level 2</a></li>
-			              <li><a href="#"><i class="fa fa-bolt" aria-hidden="true"></i> Level 2</a></li>
-			              <li><a href="/level-one" class="go-back">Back</a></li>
-			            </ul>
-
-			          <li><a href="#"><i class="fa fa-bar-chart" aria-hidden="true"></i> Level 1</a></li>
-			          <li><a href="#"><i class="fa fa-bar-chart" aria-hidden="true"></i> Level 1</a></li>
-			          <li><a href="/menu" class="go-back">Back</a></li>
-			        </ul>
-
-			      <li><a href="#" class="navbar-content-item"><i class="fa fa-archive" aria-hidden="true"></i> Link 2</a></li>
-			      <li><a href="#" class="navbar-content-item"><i class="fa fa-archive" aria-hidden="true"></i> Link 3</a></li>
-			      <li><a href="#" class="navbar-content-item"><i class="fa fa-archive" aria-hidden="true"></i> Link 4</a></li>
-			    </ul>
-		  	</div> -->
 		</div>
 
 		<ul class="breadcrumbs"></ul>
