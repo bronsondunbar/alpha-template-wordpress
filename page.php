496a23c047d9daa8c95a4cbf89d3e2d9b3ae9706
Bronson Dunbar <bronson@bronsondunbar.com>
2018-02-06 21:34:13 +0200

    Updates

--- page.php
+++ page.php
@@ -17,25 +17,23 @@
 
 get_header(); ?>
 
-<div class="wrap">
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<section role="post">
+ 	<div class="container waypoint">
 
-			<?php
-			while ( have_posts() ) : the_post();
+		<?php
+		while ( have_posts() ) : the_post();
 
-				get_template_part( 'template-parts/page/content', 'page' );
+			get_template_part( 'template-parts/page/content', 'page' );
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
 
-			endwhile; // End of the loop.
-			?>
+		endwhile; // End of the loop.
+		?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
-</div><!-- .wrap -->
+	</div>
+</div>
 
 <?php get_footer();
---
+++ template-parts/page/content-page.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+
+<?php
+	the_content();
+?>
